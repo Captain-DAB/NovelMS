c8e9620110733c63865d226c43e9e53abc948baa
Captain-DAB <115474437+Captain-DAB@users.noreply.github.com>
2024-03-12 11:05:46 +0100

    Update projects

--- src/novelms/FXMLDocumentController.java
+++ src/novelms/FXMLDocumentController.java
@@ -6,22 +6,86 @@ package novelms;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 /**
  *
  * @author USER
  */
 public class FXMLDocumentController implements Initializable {
+
+    @FXML
+    private Button alreadyHave;
+
+    @FXML
+    private Button createBtn;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private AnchorPane register_form;
+
+    @FXML
+    private AnchorPane welcome;
     
-    
-    
+    public void switchForm(ActionEvent event) {
+
+        TranslateTransition slider = new TranslateTransition();
+
+        if (event.getSource() == createBtn) {
+            slider.setNode(welcome);
+            slider.setToX(350);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((ActionEvent e) -> {
+                alreadyHave.setVisible(true);
+                createBtn.setVisible(false);
+                
+                register_form.setVisible(true);
+                login_form.setVisible(false);
+                
+                welcome.setStyle("-fx-background-color: #30343E; -fx-background-radius: 0 10px 10px 0;");
+
+//                fp_questionForm.setVisible(false);
+//                si_loginForm.setVisible(true);
+//                np_newPassForm.setVisible(false);
+
+//                regLquestionList();
+            });
+            slider.play();
+        } else if (event.getSource() == alreadyHave) {
+            slider.setNode(welcome);
+            slider.setToX(0);
+            slider.setDuration(Duration.seconds(.5));
+
+            slider.setOnFinished((ActionEvent e) -> {
+                alreadyHave.setVisible(false);
+                createBtn.setVisible(true);
+                
+                register_form.setVisible(false);
+                login_form.setVisible(true);
+                
+                welcome.setStyle("-fx-background-color: #30343E; -fx-background-radius: 10px 0 0 10px;");
+
+//                fp_questionForm.setVisible(false);
+//                si_loginForm.setVisible(true);
+//                np_newPassForm.setVisible(false);
+            });
+            slider.play();
+        }
+
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
