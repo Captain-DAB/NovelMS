8b24d8ed944e2c99f42bc2f81e9e630537487404
Captain-DAB <115474437+Captain-DAB@users.noreply.github.com>
2024-03-14 08:20:30 +0100

    Login Page Done

--- src/novelms/FXMLDocumentController.java
+++ src/novelms/FXMLDocumentController.java
@@ -15,14 +15,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 /**
@@ -59,6 +64,9 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane fp_questionForm;
 
     @FXML
+    private TextField fp_username;
+
+    @FXML
     private Label loginAccount;
 
     @FXML
@@ -80,10 +88,10 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane np_changePasswordForm;
 
     @FXML
-    private TextField np_confirmPassword;
+    private PasswordField np_confirmPassword;
 
     @FXML
-    private TextField np_newPassword;
+    private PasswordField np_newPassword;
 
     @FXML
     private Label registerAccount;
@@ -95,7 +103,7 @@ public class FXMLDocumentController implements Initializable {
     private AnchorPane register_form;
 
     @FXML
-    private TextField si_password;
+    private PasswordField si_password;
 
     @FXML
     private TextField si_username;
@@ -104,7 +112,7 @@ public class FXMLDocumentController implements Initializable {
     private TextField su_answer;
 
     @FXML
-    private TextField su_password;
+    private PasswordField su_password;
 
     @FXML
     private ComboBox<?> su_question;
@@ -164,20 +172,20 @@ public class FXMLDocumentController implements Initializable {
                     alert.setContentText("Successfully login!");
                     alert.showAndWait();
 
-//                    //Link your MainForm
-//                    Parent root = FXMLLoader.load(getClass().getResource("mainForm.fxml"));
-//                    
-//                    Stage stage = new Stage();
-//                    Scene scene = new Scene(root);
-//                    
-//                    stage.setTitle("Sneaker Inventory Management System");
-//                    stage.setMinWidth(1100);
-//                    stage.setMinHeight(600);
-//                    
-//                    stage.setScene(scene);
-//                    stage.show();
-//                    
-//                    loginBtn.getScene().getWindow().hide();
+                    //Link your MainForm
+                    Parent root = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    
+                    Stage stage = new Stage();
+                    Scene scene = new Scene(root);
+                    
+                    stage.setTitle("Novel Management System Dashboard");
+                    stage.setMinWidth(1100);
+                    stage.setMinHeight(600);
+                    
+                    stage.setScene(scene);
+                    stage.show();
+                    
+                    loginBtn.getScene().getWindow().hide();
 //                    
                 } else {//IF NOT, THEN THE ERROR MESSAGE WILL APPEAR
                     alert = new Alert(Alert.AlertType.ERROR);
@@ -268,11 +276,139 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
+    public void proceedBtn() {
+        if (fp_username.getText().isEmpty() || fp_question.getSelectionModel().getSelectedItem() == null
+                || fp_answer.getText().isEmpty()) {
+
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+        } else {
+            String selectData = "SELECT username, question, answer FROM admin WHERE username = ? AND question = ? AND answer = ?";
+            connect = database.connectDB();
+
+            try {
+
+                prepare = connect.prepareStatement(selectData);
+                prepare.setString(1, fp_username.getText());
+                prepare.setString(2, (String) fp_question.getSelectionModel().getSelectedItem());
+                prepare.setString(3, fp_answer.getText());
+
+                result = prepare.executeQuery();
+
+                if (result.next()) {
+                    np_changePasswordForm.setVisible(true);
+                    fp_questionForm.setVisible(false);
+
+                } else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Incorrect Information");
+                    alert.showAndWait();
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void forgotPassQuestionList() {
+
+        List<String> listQ = new ArrayList<>();
+
+        for (String data : questionList) {
+            listQ.add(data);
+        }
+
+        ObservableList listData = FXCollections.observableArrayList(listQ);
+        fp_question.setItems(listData);
+    }
+
+    public void changePassBtn() {
+
+        if (np_newPassword.getText().isEmpty() || np_confirmPassword.getText().isEmpty()) {
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill all blank fields");
+            alert.showAndWait();
+        } else {
+
+            if (np_newPassword.getText().equals(np_confirmPassword.getText())) {
+                String getDate = "SELECT date FROM admin WHERE username = '"
+                        + fp_username.getText() + "'";
+
+                connect = database.connectDB();
+
+                try {
+                    prepare = connect.prepareStatement(getDate);
+                    result = prepare.executeQuery();
+
+                    String date = "";
+                    if (result.next()) {
+                        date = result.getString("date");
+                    }
+
+                    String updatePass = "UPDATE admin SET password = '"
+                            + np_newPassword.getText() + "', question = '"
+                            + fp_question.getSelectionModel().getSelectedItem() + "', answer = '"
+                            + fp_answer.getText() + "', date = '"
+                            + date + "' WHERE username = '"
+                            + fp_username.getText() + "'";
+
+                    prepare = connect.prepareStatement(updatePass);
+                    prepare.executeUpdate();
+
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully changed Password!");
+                    alert.showAndWait();
+
+                    login_form.setVisible(true);
+                    np_changePasswordForm.setVisible(false);
+
+                    //TO CLEAR FIELDS
+                    np_newPassword.setText("");
+                    np_confirmPassword.setText("");
+                    fp_question.getSelectionModel().clearSelection();
+                    fp_answer.setText("");
+                    fp_username.setText("");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error Message");
+                alert.setHeaderText(null);
+                alert.setContentText("Password Mismatch");
+                alert.showAndWait();
+            }
+
+        }
+
+    }
+
+    public void backToLoginForm() {
+        login_form.setVisible(true);
+        fp_questionForm.setVisible(false);
+    }
+
+    public void backToQuestionForm() {
+        fp_questionForm.setVisible(true);
+        np_changePasswordForm.setVisible(false);
+    }
+
     public void switchForgotPass() {
         fp_questionForm.setVisible(true);
         login_form.setVisible(false);
 
-//        forgotPassQuestionList();
+        forgotPassQuestionList();
     }
 
     public void switchForm(ActionEvent event) {
